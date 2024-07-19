236eadd6f954b03bc0efdf33fc74582747a2ffff
Doan Tran <nhadoan1212@gmail.com>
2024-07-19 10:50:02 -0400

    Update buttons

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -11,62 +11,65 @@ const BaseButton = ({ children, onClick, className, ...props }) => {
 }
 
 const ButtonType = {
-    gradient: 'btnGradient',
-    solid: 'btnSolid',
-    outline: 'btnOutline',
-    'gradient-outline': 'btnGradientOutline',
+    'solid-primary':
+        'px-5 py-2 bg-wisteria-600 hover:bg-wisteria-700 focus:bg-wisteria-800 text-white',
+    'solid-secondary':
+        'px-5 py-2 bg-nypink-400 hover:bg-nypink-500 focus:bg-nypink-600 text-white',
+    'outline-primary':
+        'border-[2px] px-5 py-2 border-wisteria-600 hover:border-wisteria-700 hover:bg-wisteria-700 hover:text-white focus:bg-wisteria-800',
+    'outline-secondary':
+        'border-[2px] px-5 py-2 border-nypink-400 hover:border-nypink-500 hover:bg-nypink-500 hover:text-white focus:bg-nypink-600',
+    'gradient-primary':
+        'text-white px-5 py-2 bg-gradient-to-r from-wisteria-600 to-nypink-400',
+    'gradient-secondary':
+        'text-white px-5 py-2 bg-gradient-to-r from-nypink-400 to-wisteria-600',
 }
 
-const ButtonColor = {
-    solid: {
-        primary: 'btnSolidPrimary',
-        secondary: 'btnSolidSecondary',
-    },
-    gradient: {
-        primary: 'btnGradientPrimary',
-    },
-    outline: {
-        primary: 'btnOutlinePrimary',
-        secondary: 'btnOutlineSecondary',
-    },
-    'gradient-outline': {
-        primary: 'btnGradientOutlinePrimary',
-    },
-}
+const Button = ({ type, className, children, onClick, ...props }) => {
+    const btnClass = ButtonType[type]
+        ? ButtonType[type]
+        : ButtonType['solid-primary']
 
-const ButtonTransition = {
-    basic: 'basicTransition',
-}
-
-const buttonClassGenerator = (type, color, transition) => {
-    let btnType = ButtonType[type] ? ButtonType[type] : ButtonType.solid
-    let btnColor = ButtonColor[type][color] ? ButtonColor[type][color] : ''
-    let btnTransition = ButtonTransition[transition]
-        ? ButtonTransition[transition]
-        : ''
-    return `${btnType} ${btnColor} ${btnTransition}`
-}
-
-/*
- * type: gradient, solid, outline
- * gradientFrom: color
- * gradientTo: color
- * color: color
- * transition: basic
- */
-const Button = ({ type, color, transition, children, onClick, ...props }) => {
-    const buttonClass = buttonClassGenerator(type, color, transition)
     return (
-        <BaseButton onClick={onClick} className={buttonClass} {...props}>
-            {type === 'gradient-outline' ? (
-                <span className="textGradientOutlinePrimary basicTransition">
-                    {children}
-                </span>
-            ) : (
-                <span>{children}</span>
-            )}
+        <BaseButton
+            onClick={onClick}
+            className={`${className} ${btnClass} basicTransition`}
+            {...props}
+        >
+            {children}
         </BaseButton>
     )
 }
 
-export { BaseButton, Button }
+const GradientButtonType = {
+    primary:
+        'text-white p-[2px] bg-gradient-to-r from-wisteria-600 to-nypink-400',
+    secondary:
+        'text-white p-[2px] bg-gradient-to-r from-nypink-400 to-wisteria-600',
+}
+
+const OutlineGradientButton = ({
+    type,
+    className,
+    children,
+    onClick,
+    ...props
+}) => {
+    const btnClass = GradientButtonType[type]
+        ? GradientButtonType[type]
+        : GradientButtonType['primary']
+
+    return (
+        <BaseButton
+            onClick={onClick}
+            className={`${className} ${btnClass}`}
+            {...props}
+        >
+            <span className="basicTransition text-wisteria-950 block rounded-full bg-white px-5 py-2 font-eloquialight group-hover:bg-transparent group-hover:text-white">
+                {children}
+            </span>
+        </BaseButton>
+    )
+}
+
+export { Button, OutlineGradientButton }
--- src/Tabs/Home/Home.js
+++ src/Tabs/Home/Home.js
@@ -1,18 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { Button } from '../../Components/Button/Button'
- 
+import { Button, OutlineGradientButton } from '../../Components/Button/Button'
+
 const Home = () => {
     return (
-        <div className='flex flex-row flex-wrap gap-3'>
-            <Button type="gradient" color="primary" children={"Gradient Primary"} transition={"basic"}/>
-            <Button type="gradient-outline" color="primary" children={"Gradient Outline Primary"} transition={"basic"}/>
-            <Button type="solid" color="primary" children={"Solid Primary"} transition={"basic"}/>
-            <Button type="solid" color="secondary" children={"Solid Primary"} transition={"basic"}/>
-            <Button type="outline" color="primary" children={"Outline Primary"} transition={"basic"}/>
-            <Button type="outline" color="secondary" children={"Outline Primary"} transition={"basic"}/>
+        <div className="flex flex-row flex-wrap gap-3">
+            <Button type="solid-primary" children={'Solid Primary'} />
+            <Button type="solid-secondary" children={'Solid Primary'} />
+            <Button type="outline-primary" children={'Outline Primary'} />
+            <Button type="outline-secondary" children={'Outline Primary'} />
+            <OutlineGradientButton
+                type="primary"
+                children={'Outline Gradient Primary'}
+            />
+            <OutlineGradientButton
+                type="secondary"
+                children={'Outline Gradient Secondary'}
+            />
         </div>
     )
 }
- 
+
 export default Home
