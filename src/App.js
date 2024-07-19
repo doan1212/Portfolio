d93384dd1010faa3f466b491fd2667682db801fc
Doan Tran <nhadoan1212@gmail.com>
2024-07-18 23:19:34 -0400

    add basic navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css'
+import { Link, Route, Routes } from 'react-router-dom'
+import Home from './Tabs/Home/Home'
+import About from './Tabs/About/About'
 
 function App() {
-  return (
-    <div>
-      <h1 class="text-2xl font-bold underline">Hello Doan!</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'space-evenly',
+                        background: 'green',
+                        fontSize: '20px',
+                    }}
+                >
+                    <Link to={'/'} style={{ color: 'white' }}>
+                        Home
+                    </Link>
+                    <Link
+                        to={'/about'}
+                        style={{
+                            color: 'white',
+                        }}
+                    >
+                        About
+                    </Link>
+                </div>
+            </div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </div>
+    )
 }
 
-export default App;
+export default App
