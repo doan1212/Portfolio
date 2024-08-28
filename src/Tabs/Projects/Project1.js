b83ca8674105e0f4bbc8560fb43dd6c262e243b8
Doan Tran <nhadoan1212@gmail.com>
2024-08-28 01:52:21 -0400

    fix nav bar

--- src/Tabs/Projects/Project1.js
+++ src/Tabs/Projects/Project1.js
@@ -46,24 +46,9 @@ const Project1 = () => {
     return (
         <div>
             <SectionPrimary>
-                {/* <div className="flex flex-col items-center justify-start gap-5 md:flex-row">
-                    <div className="items-center justify-start p-5">
-                        <div className="font-laviossa text-5xl text-wisteria-600">
-                            {DATA.name}
-                        </div>
-                        <div className="w-96 font-eloquialight text-xl text-emperor-700">
-                            {DATA.shortDescription}
-                        </div>
-                    </div>
-                    <img
-                        className="h-3/4 w-3/4"
-                        src={banner}
-                        alt="ILUVUS"
-                    />
-                </div> */}
-                <div className="lg:grid lg:grid-cols-8 gap-5">
+                <div className="gap-5 lg:grid lg:grid-cols-8">
                     <div className="lg:col-span-3 lg:col-start-1">
-                        <div className="flex h-full flex-col items-start justify-center p-5">
+                        <div className="flex h-full flex-col items-center justify-center p-5 lg:items-start">
                             <div className="font-laviossa text-5xl text-wisteria-600">
                                 {DATA.name}
                             </div>
@@ -72,7 +57,7 @@ const Project1 = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="lg:col-span-5 lg:col-start-4 flex items-center justify-center">
+                    <div className="flex items-center justify-center lg:col-span-5 lg:col-start-4">
                         <img
                             className="h-3/4 w-auto"
                             src={banner}
@@ -236,7 +221,7 @@ const Project1 = () => {
                         DATA.sections.design['styleGuide'].description
                     )}
                 />
-                
+
                 <ImageSlider images={guides}> </ImageSlider>
             </SectionSecondary>
 
