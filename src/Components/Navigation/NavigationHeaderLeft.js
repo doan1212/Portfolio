128cd76cc88d88d749d72444fec99bbf670f8d88
Doan <nhadoan1212@gmail.com>
2025-03-19 01:35:05 -0400

    update home button

--- src/Components/Navigation/NavigationHeaderLeft.js
+++ src/Components/Navigation/NavigationHeaderLeft.js
@@ -1,8 +1,15 @@
+
+import { useNavigate } from 'react-router-dom';
+
 const NavigationHeaderLeft = () => {
+    const navigate = useNavigate();
     return (
-        <div className="items-start justify-start font-amsterdam text-[1.5em] text-emperor-700">
+        <button 
+            className="items-start justify-start font-amsterdam text-[1.5em] text-emperor-700 hover:text-emperor-900 focus:outline-none"
+            onClick={() => navigate("/")}
+        >
             Doan
-        </div>
+        </button>
     )
 }
 
