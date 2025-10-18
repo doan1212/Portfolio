3582b954068a3c232c8f8a64aefd27d093f2b0b1
Doan <nhadoan1212@gmail.com>
2025-10-18 04:40:25 -0400

    update campfy

--- src/Tabs/Projects/Project4.js
+++ src/Tabs/Projects/Project4.js
@@ -4,10 +4,12 @@ import {
 } from '../../Components/Section/Section'
 import mock4 from '../../Assets/Image/mock_4.png'
 import ScrollToSection from '../../Components/Section/ScrollToSection'
-import Wireframe from '../../Assets/Image/wireframep3.jpg'
-
-import comparative from '../../Assets/Image/CAproject3.png'
-import styleGuide from '../../Assets/Image/StyleGuide3.png'
+import userstory1 from '../../Assets/Image/Story1.jpg'
+import userstory2 from '../../Assets/Image/Story2.jpg'
+import userstory3 from '../../Assets/Image/Story3.jpg'
+import styleGuide from '../../Assets/Image/StyleGuide4.png'
+import paperWireframe from '../../Assets/Image/paper1.jpg'
+import paperWireframe2 from '../../Assets/Image/paper2.jpg'
 import { formatString } from '../../Utils/stringHelper'
 import {
     VerticalTextBox,
@@ -17,38 +19,33 @@ import {
 import { ImageCardWithDescription } from '../../Components/ImageCard/ImageCard'
 import { TwoImageCardWithDescription } from '../../Components/ImageCard/ImageCard'
 import { ImageSlider } from '../../Components/ImageCard/ImageSlider'
-import userflow1 from '../../Assets/Image/userflow1.png'
-import userflow2 from '../../Assets/Image/userflow2.png'
-import userflow3 from '../../Assets/Image/userflow3.png'
-import userflow4 from '../../Assets/Image/userflow4.png'
-import guide1 from '../../Assets/Image/guide1.png'
-import guide2 from '../../Assets/Image/guide2.png'
-import guide3 from '../../Assets/Image/guide3.png'
-import guide4 from '../../Assets/Image/guide4.png'
-import guide5 from '../../Assets/Image/guide5.png'
 import { useState, useEffect } from 'react'
 import { FaSquareGithub } from 'react-icons/fa6'
-import UserPersona1 from '../../Assets/Image/UserPersona1.png'
-import UserPersona2 from '../../Assets/Image/UserPersona2.png'
-import note1 from '../../Assets/Image/Note1.png'
-import { i } from 'framer-motion/client'
+import UserPersona1 from '../../Assets/Image/user1.png'
+import UserPersona2 from '../../Assets/Image/user2.png'
+import UserPersona3 from '../../Assets/Image/user3.png'
+import sitemap from '../../Assets/Image/sitemap.jpg'
 import DATA from '../../Assets/Projects/project4.json'
+import userJourney1 from '../../Assets/Image/userJourney1.png'
+import { s, u } from 'framer-motion/client'
+import userflow from '../../Assets/Image/userflow.png'
 
 const Project4 = () => {
-    const images = [
-        { src: userflow1, alt: 'Userflow1' },
-        { src: userflow2, alt: 'Userflow2' },
-        { src: userflow3, alt: 'Userflow3' },
-        { src: userflow4, alt: 'Userflow4' },
+    
+
+    const UserPersonas = [
+        { src: UserPersona1, alt: 'UserPersona1' },
+        { src: UserPersona2, alt: 'UserPersona2' },
+        { src: UserPersona3, alt: 'UserPersona3' },
     ]
 
-    const guides = [
-        { src: guide1, alt: 'Guide1' },
-        { src: guide2, alt: 'Guide2' },
-        { src: guide3, alt: 'Guide3' },
-        { src: guide4, alt: 'Guide4' },
-        { src: guide5, alt: 'Guide5' },
+    const userStorys = [
+        { src: userstory1, alt: 'UserStory1' },
+        { src: userstory2, alt: 'UserStory2' },
+        { src: userstory3, alt: 'UserStory3' },
     ]
+
+    const userJourneys = [{ src: userJourney1, alt: 'UserJourney1' }]
 
     const [isFlying, setIsFlying] = useState(true)
 
@@ -106,7 +103,7 @@ const Project4 = () => {
             <SectionSecondary>
                 <div className="grid grid-cols-1 items-start gap-5 md:grid-cols-2 md:flex-row">
                     <HorizontalTextBox
-                        title="Problem"
+                        title="Overview"
                         content={formatString(DATA.overview)}
                     />
                     <HorizontalTextBox
@@ -182,17 +179,39 @@ const Project4 = () => {
                 title={DATA.sections.discovery.title}
             >
                 <VerticalTextBox
-                    title={DATA.sections.discovery.competitiveAnalysis.title}
+                    title={DATA.sections.discovery['goalStatement'].title}
                     content={formatString(
-                        DATA.sections.discovery.competitiveAnalysis.description
+                        DATA.sections.discovery['goalStatement'].description
                     )}
                 />
-                <ImageCardWithDescription
+                <HorizontalTextBox
+                    title={DATA.sections.discovery['competitiveAnalysis'].title}
                     content={formatString(
-                        DATA.sections.discovery.competitiveAnalysis.summary
+                        DATA.sections.discovery['competitiveAnalysis']
+                            .description
                     )}
-                    img={comparative}
                 />
+
+                <HorizontalTextBox
+                    title={DATA.sections.discovery['Userpersonas'].title}
+                    content={DATA.sections.discovery.Userpersonas.description}
+                />
+                <ImageSlider images={UserPersonas} />
+                <HorizontalTextBox
+                    title={DATA.sections.discovery.userStory.title}
+                    content={formatString(
+                        DATA.sections.discovery.userStory.description
+                    )}
+                />
+                <ImageSlider images={userStorys} />
+
+                <HorizontalTextBox
+                    title={DATA.sections.discovery.userJourney.title}
+                    content={formatString(
+                        DATA.sections.discovery.userJourney.description
+                    )}
+                />
+                <ImageSlider images={userJourneys} />
             </SectionSecondary>
 
             <SectionSecondary
@@ -200,42 +219,72 @@ const Project4 = () => {
                 title={DATA.sections.ideation.title}
             >
                 <HorizontalTextBox
-                    title={DATA.sections.ideation['Userpersonas'].title}
+                    title={DATA.sections.ideation.Brainstorming.title}
                     content={
-                        <TwoImageCardWithDescription
-                            content={formatString(
-                                DATA.sections.ideation['Userpersonas']
-                                    .description
-                            )}
-                            img1={UserPersona1}
-                            img2={UserPersona2}
-                        />
-                    }
+                        formatString(
+                            DATA.sections.ideation.Brainstorming.description
+                        )}
                 />
-
+                <iframe
+                    src={DATA.sections.ideation.Brainstorming.src}
+                    className="flex md:w-full"
+                    height={700}
+                    title="iframe"
+                    allowfullscreen="true"
+                />
                 <HorizontalTextBox
-                    title={DATA.sections.ideation['Wireframe'].title}
-                    content={
-                        <ImageCardWithDescription
-                            content={formatString(
-                                DATA.sections.ideation.Wireframe.description
-                            )}
-                            img={Wireframe}
-                        />
-                    }
-                />
-
-                <ImageCardWithDescription
+                    title={DATA.sections.ideation.siteMap.title}
+                    content={ <ImageCardWithDescription
                     content={formatString(
-                        DATA.sections.ideation['Note'].description
+                        DATA.sections.ideation.siteMap.description
                     )}
-                    img={note1}
+                    img={sitemap}
+                /> }
                 />
+                <HorizontalTextBox
+                    title={DATA.sections.ideation.userFlow.title}
+                    content={ <ImageCardWithDescription
+                    content={formatString(
+                        DATA.sections.ideation.userFlow.description
+                    )}
+                    img={userflow}
+                /> }
+                />
+                
+            
             </SectionSecondary>
+        
             <SectionSecondary
                 id={DATA.sections.design.id}
                 title={DATA.sections.design.title}
             >
+                <HorizontalTextBox
+                    title={DATA.sections.design['Wireframe'].title}
+                    content={formatString(
+                        DATA.sections.design['Wireframe'].description
+                    )}
+                />
+                <ImageSlider
+                    images={[
+                        { src: paperWireframe, alt: 'PaperWireframe1' },
+                        { src: paperWireframe2, alt: 'PaperWireframe2' },
+                    ]}
+                />
+
+
+                <HorizontalTextBox
+                    title={DATA.sections.design['Digital Wireframe'].title}
+                    content={formatString(
+                        DATA.sections.design['Digital Wireframe'].description
+                    )}
+                />
+                <iframe
+                    src={DATA.sections.design['Digital Wireframe'].src}
+                    className="flex md:w-full"
+                    height={700}
+                    title="iframe"
+                    allowfullscreen="true"
+                />
                 <HorizontalTextBox
                     title={DATA.sections.design['styleGuide'].title}
                     content={formatString(
@@ -255,13 +304,13 @@ const Project4 = () => {
                 id={DATA.sections.development.id}
                 title={DATA.sections.development.title}
             >
-                <HorizontalTextBox
+                {/* <HorizontalTextBox
                     title={DATA.sections.development['prototype'].title}
                     content={formatString(
                         DATA.sections.development['prototype'].description
                     )}
                 />
-                {/* <div className="flex justify-center">
+                 <div className="flex justify-center">
                     <div
                         style={{
                             transform: 'scale(0.4)',
@@ -270,7 +319,7 @@ const Project4 = () => {
                     >
                         <ImageSlider images={prototypes} />
                     </div>
-                </div> */}
+                </div> 
 
                 <iframe
                     src={DATA.sections.development.prototype.src}
@@ -278,14 +327,14 @@ const Project4 = () => {
                     height={700}
                     title="iframe"
                     allowfullscreen="true"
-                />
+                />  */}
             </SectionSecondary>
 
             <SectionSecondary
                 id={DATA.sections.reflection.id}
                 title={DATA.sections.reflection.title}
             >
-                <div className="grid grid-cols-1 items-start gap-5 md:grid-cols-2 md:flex-row">
+                {/* <div className="grid grid-cols-1 items-start gap-5 md:grid-cols-2 md:flex-row">
                     <HorizontalTextBox
                         title={DATA.sections.reflection['lookingback'].title}
                         content={
@@ -317,7 +366,7 @@ const Project4 = () => {
                                 .description
                         )}
                     />
-                </div>
+                </div> */}
             </SectionSecondary>
         </div>
     )
